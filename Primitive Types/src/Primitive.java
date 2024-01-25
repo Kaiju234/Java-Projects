8c0d84b442438d0908f10b716ebfd4a4b49d755f
Raj Shah <68970395+Kaiju234@users.noreply.github.com>
2024-01-25 09:59:36 -0600

    Add files via upload

---
+++ Primitive Types/src/Primitive.java
@@ -0,0 +1,14 @@
+public class Primitive {
+
+   public static void main(String[] args) {
+      byte views = 30;
+      long viewsCount = 3_100_1223_345L;
+      double price = 10.99;
+      float total = 10.999F;
+      char letter = 'A';
+      boolean isEligibile = false;
+   }
+
+}
+
+
---
+++ Variables/src/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+
+public static void main(String[] args) {
+    int myAge =  19;
+    int herAge = myAge;
+    System.out.println(herAge);
+    }
+}
+
