c7896208a8f73d63c4d02d22c2fc49cc785ea444
Raj Shah <68970395+Kaiju234@users.noreply.github.com>
2024-01-25 10:28:21 -0600

    Add files via upload

---
+++ Reference Types/src/Refrence.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Date;
+public class Refrence {
+
+    public static void main(String[] args) {
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
+
+    }
+
+
+}
